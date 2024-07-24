4bc3e67b98c4bd79762d37be3f43c26c235d0907
Philip Hayes <philip@deewhy.ie>
2024-07-24 17:52:18 -0400

    minor edit

--- prompt-testing/index.test.js
+++ prompt-testing/index.test.js
@@ -7,7 +7,7 @@ const callModel = prompt.callModel;
 describe('Parasol model training tests', () => {
   test('should pass when model is asked what an insurance policy', async () => {
     await expect(await callModel('What is an insurance policy')).toMatchSemanticSimilarity(
-      'An insurance policy is a contract between an individual or entity (the insured) and an insurance company (the insurer). The contract outlines the terms, conditions, and coverage provided by the insurer in exchange for premium payments made by the insured. The policy defines the risks that are covered, the limits and exclusions of coverage, deductibles, and any other relevant details. In the event of a covered loss, the insurer is obligated to provide financial compensation to the insured, as specified in the policy',
+      'An insurance policy is a contract between an individual or entity (the insured) and an insurance company (the insurer). The contract outlines the terms, conditions, and coverage provided by the insurer in exchange for premium payments made by the insured. The policy defines the risks that are covered, the limits and exclusions of coverage, deductibles, and any other relevant details. In the event of a covered loss, the insurer is obligated to provide financial compensation to the insured, as specified in the policy.',
       0.9
     );
   }, 10000);

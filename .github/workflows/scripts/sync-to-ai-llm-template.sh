d36f9cbc29463c74b02cb96cb108421815aa837e
Eric Deandrea <eric.deandrea@gmail.com>
2024-09-06 14:22:58 -0400

    Sync templates on commit

--- .github/workflows/scripts/sync-to-ai-llm-template.sh
+++ .github/workflows/scripts/sync-to-ai-llm-template.sh
@@ -21,6 +21,13 @@ sync_prompt_testing() {
   cp -Rfv $prompt_testing_source/ $prompt_testing_target
 }
 
+replace_in_file() {
+  local filename=$1
+  local search_replace_string=$2
+
+  sed -er "$search_replace_string" "$filename" > "${filename}.new" && mv -- "${filename}.new" "$filename"
+}
+
 sync_app() {
   local app_source=$source_root/app
   local app_target=$target_directory
@@ -29,22 +36,25 @@ sync_app() {
   # 1st copy everything
   cd $app_source && \
     tar --exclude="src/main/webui/node_modules" \
-    --exclude="src/main/docker" \
-    --exclude="target" \
-    --exclude=".quinoa" \
-    --exclude="*.iml" \
-    --exclude=".dockerignore" \
-    --exclude=".gitignore" \
-    -cf - . | tar -xf - -C $app_target
-#  tar -cf - --exclude="$app_source/src/main/webui/node_modules" --exclude="$app_source/src/main/docker" --exclude="$app_source/target" --exclude="$app_source/.quinoa" --exclude="$app_source/*.iml" $app_source | tar -xvf - -C $app_target
-#  cp -Rfv $app_source/ $app_target
+      --exclude="src/main/docker" \
+      --exclude="target" \
+      --exclude=".quinoa" \
+      --exclude="*.iml" \
+      --exclude=".dockerignore" \
+      --exclude=".gitignore" \
+      -cf - . | \
+    tar -xf - -C $app_target
 
   # Now replace whatever needs replacing in $app_source/src/main/resources/application.properties
   app_props_file=$app_target/src/main/resources/application.properties
-  sed -i '' -r "s/^[#]*\s*quarkus.http.port=.*/quarkus.http.port=\${{values.port}}/" $app_props_file
-  sed -i '' -r "s/^[#]*\s*quarkus.langchain4j.openai.parasol-chat.base-url=.*/quarkus.langchain4j.openai.parasol-chat.base-url=http:\/\/parasol-chat-predictor.aiworkshop.svc.cluster.local:8080\/v1/" $app_props_file
-  sed -i '' -r "s/^quarkus.tls.trust-all/# quarkus.tls.trust-all/" $app_props_file
-  sed -i '' -r "s/^quarkus.dev-ui.hosts/# quarkus.dev-ui.hosts/" $app_props_file
+  replace_in_file "$app_props_file" "s/^[#]*\s*quarkus.http.port=.*/quarkus.http.port=\${{values.port}}/"
+  replace_in_file "$app_props_file" "s/^[#]*\s*quarkus.langchain4j.openai.parasol-chat.base-url=.*/quarkus.langchain4j.openai.parasol-chat.base-url=http:\/\/parasol-chat-predictor.aiworkshop.svc.cluster.local:8080\/v1/"
+  replace_in_file "$app_props_file" "s/^quarkus.tls.trust-all/# quarkus.tls.trust-all/"
+  replace_in_file "$app_props_file" "s/^quarkus.dev-ui.hosts/# quarkus.dev-ui.hosts/"
+#  sed -i '' -r "s/^[#]*\s*quarkus.http.port=.*/quarkus.http.port=\${{values.port}}/" $app_props_file
+#  sed -i '' -r "s/^[#]*\s*quarkus.langchain4j.openai.parasol-chat.base-url=.*/quarkus.langchain4j.openai.parasol-chat.base-url=http:\/\/parasol-chat-predictor.aiworkshop.svc.cluster.local:8080\/v1/" $app_props_file
+#  sed -i '' -r "s/^quarkus.tls.trust-all/# quarkus.tls.trust-all/" $app_props_file
+#  sed -i '' -r "s/^quarkus.dev-ui.hosts/# quarkus.dev-ui.hosts/" $app_props_file
 }
 
 sync_prompt_testing

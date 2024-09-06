e3be89621db9c32f2d4255e4be5055edf4a03009
Philip Hayes <philip@deewhy.ie>
2024-09-06 14:11:39 -0400

    Updated tests to match scaffold from rhdh template (#83)
    * Update to jest config
    
    * added claim summary test
    
    * removed package-lock.json
    
    * Updated tests to work with trained model
    
    * Updated tests to match scaffold from rhdh template
    
    * update to settings for new model urls
    
    ---------
    
    Co-authored-by: Philip Hayes <phayes@phayes-mac.lan>

--- prompt-testing/index.test.js
+++ prompt-testing/index.test.js
@@ -5,33 +5,18 @@ const callModel = prompt.callModel;
 
 
 describe('Parasol model training tests', () => {
-  test('should pass when model is asked who founded parasol insurance', async () => {
-    await expect(await callModel('Who founded parasol insurance? ')).toMatchSemanticSimilarity(
-      'Parasol Insurance was founded in 1936 by James Falkner and James Labocki. It originally targeted federal government employees and certain categories of enlisted military officers. Recently, it has started to focus on the RAD AI Roadshow and provides enablement workshops all around the world. Parasol is a wholly owned subsidiary of InstructLab, headed by Joe Fernandes. Joe Fernandes acquired Parasol in 2024. The company is headquartered in Orlando, Florida.',
+  test('should pass when model is asked what an insurance policy', async () => {
+    await expect(await callModel('What is an insurance policy? ')).toMatchSemanticSimilarity(
+      'An insurance policy is a contract between an individual or entity (the insured) and an insurance company (the insurer). The contract outlines the terms, conditions, and coverage provided by the insurer in exchange for premium payments made by the insured. The policy defines the risks that are covered, the limits and exclusions of coverage, deductibles, and any other relevant details. In the event of a covered loss, the insurer is obligated to provide financial compensation to the insured, as specified in the policy.',
       0.9
     );
   }, 10000);
-  test('should pass when model is asked what is Parasol insurance', async () => {
-    await expect(await callModel('Are parasol insurance customers covered for uninsured drivers?')).toMatchSemanticSimilarity(
-      "Parasol insurance customers who have the optional Apex plus package are eligible for reimbursement for loss of income if they are at fault in an incident with an uninsured driver.   ",
+  test('should pass when model is asked who founded Parasol insurance', async () => {
+    await expect(await callModel('In one sentence, who founded Parasol Insurance? ')).toMatchSemanticSimilarity(
+      "Parasol Insurance was founded in 1936 by James Falkner and James Labocki.",
       0.9
     );
   }, 10000);
-});
-
-describe('Parasol Claim response test', () => {
-  const prompt = `I was recently involved in a car accident.  The other driver was at fault but does not have insurance.  What can I do? Does my policy cover uninsured motorist coverage?`;
-
-  const systemPrompt = "Write a response to this customer as a Parasol Customer Service representive using knowledge about Parasol insurance. "
-
-  const response = `I'm sorry to hear about your recent car accident. In your situation, if the other driver is at fault and does not have insurance, Parasol Insurance's Apex plus package can provide reimbursement for loss of income if you have the package included in your policy. However, if the other driver is uninsured or lacks sufficient coverage to pay for your injuries or car damage, Parasol Insurance may not be able to provide full compensation for your losses.`;
-
-  test('should pass when response to claim summary is accurate', async () => {
-    await expect(await callModel(prompt, systemPrompt)).toMatchSemanticSimilarity(
-      response,
-      0.8
-    );
-  }, 20000);
 });
 
 // ADD NEW TEST HERE
